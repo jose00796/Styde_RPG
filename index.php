f87a4da25de394b9e17e50913dd202e1f927dd4e
jose00796 <josebalboa11@hotmail.com>
2020-08-21 11:39:22 -0400

    Herencia y Abstraccion Papu

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 
+require_once "proceso.php";
+
+$jose = new Soldier('Jose');
+$david = new Archer('David');
+
+$jose->move('El Norte');
+$jose->attack('David');
--- proceso.php
+++ proceso.php
@@ -1,6 +1,36 @@
 <?php
 
-class Unit
+abstract class Unit
 {
-    
+    protected $alive = true;
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function move($direccion)
+    {
+        echo "<p>{$this->name} Avanza hacia $direccion";
+    }
+
+    abstract public function attack($opponent);
 }
+
+class Soldier extends Unit
+{
+    public function attack($opponent)
+    {
+        echo "<p>{$this->name} Ataca Con una Espada a $opponent porque se comio la Luz";
+    }
+}
+
+class Archer extends Unit
+{
+    public function attack($opponent)
+    {
+        echo "<p>{$this->name} Dispara una Flecha a $opponent porque se comio la Luz";
+    }   
+}
+
